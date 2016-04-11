7bf0cbf17522d8a5d68a177a4437dd587c32e0c6
teddybear02 <theo.frohlich@gmail.com>
2016-04-11 06:20:33 -0400

    added attack

--- src/edu/cwru/sepia/action/Attack.java
+++ src/edu/cwru/sepia/action/Attack.java
@@ -9,20 +9,16 @@ public class Attack extends TargetedAction {
 
     private final Unit.UnitView F;  // friendly footman
     private final Unit.UnitView E;  // enemy footman
-    private final int f;  // friendly footman ID
-    private final int e;  // enemy footman ID
 
     public Attack(Unit.UnitView F, Unit.UnitView E) {
         super(F.getID(), ActionType.COMPOUNDATTACK, E.getID());
         this.F = F;
         this.E = E;
-        this.f = F.getID();
-        this.e = E.getID();
     }
 
     @Override
     public String toString() {
-        return "Attack(F=" + this.f + ", E=" + this.e + ")";
+        return "Attack(F=" + this.F.getID() + ", E=" + this.E.getID() + ")";
     }
 
 }
